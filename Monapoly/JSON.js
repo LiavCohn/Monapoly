444deb5b4f24ec75a7689038f96f03bb37f01a98
LiavCohn <liavcohen955@gmail.com>
2021-08-28 11:20:12 +0300

    Add project files.

---
+++ Monapoly/JSON.js
@@ -0,0 +1,388 @@
+﻿monopoly = {
+    "platforms":[
+        {   //steam platform
+            'name':'steam',
+            'cards': [
+                {
+                    genre: 'Unique',
+                    name: 'Starting point',
+                    img_src: 'images/Go.jpg'
+                },
+                //--------go
+                {
+                    genre: 'Sports',
+                    name: 'FIFA 21',
+                    cost: 250,
+                    img_src: 'https://images.daznservices.com/di/library/GOAL/ca/f3/fifa-21-kylian-mbappe_5y8kt6rcv8tf1kifrjd2ku44c.jpg?t=2054301555&quality=100',
+                    color:'IndianRed'
+                },
+                {
+                    genre: 'Unique',
+                    name: 'Tax',
+                    img_src: 'images/tax-card.jpg'
+               },
+                {
+                    genre: 'Sports',
+                    name: 'NBA 21',
+                    cost: 150,
+                    img_src: 'https://s1.gaming-cdn.com/images/products/7107/orig/nba-2k21-cover.jpg',
+                    color: 'IndianRed'
+                },
+                //-------------1 (3)
+                {
+                    genre: 'Shooting',
+                    name: 'Call Of Duty',
+                    cost: 222,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/42700/header.jpg?t=1589829053',
+                    color:'GreenYellow'
+                },
+                {
+                    genre: 'Shooting',
+                    name: 'Battlefield V',
+                    cost: 300,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1238810/header.jpg?t=1609386727',
+                    color: 'GreenYellow'
+                },
+                {
+                    genre: 'Uniqe',
+                    name: 'Jail',
+                    img_src: 'https://www.ultraboardgames.com/monopoly/gfx/ms11.jpg',
+                    color: 'MediumPurple'
+                },
+                //-----------2 (6)
+                {
+                    genre: 'Adventure',
+                    name: 'Red Dead Redemption',
+                    cost: 200,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg?t=1606935465',
+                    color:'MediumPurple'
+                },
+                {
+                    genre: 'Adventure',
+                    name: 'Devil May Cry 5',
+                    cost: 400,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/601150/header.jpg?t=1608599498',
+                    color: 'MediumPurple'
+                },
+                {
+                    genre: 'Adventure',
+                    name: "Assassin's Creed",
+                    cost: 110,
+                    img_src: 'https://steamcdn-a.akamaihd.net/steam/apps/15100/header.jpg?t=1532007211',
+                    color: 'MediumPurple'
+                },
+                //----------3 (9)
+                {
+                    genre: 'Racing',
+                    name: 'Need for Speed Heat',
+                    cost: 70,
+                    img_src: 'https://media.contentapi.ea.com/content/dam/need-for-speed/nfs-heat/common/buy/nsfh-pc-dlx-packart.png',
+                    color:'Gold'
+                },
+                {
+                    genre: 'Racing',
+                    name: 'F1 2020',
+                    cost: 90,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1080110/capsule_616x353.jpg?t=1607516630',
+                    color: 'Gold'
+                },
+                //--------4 (11)
+                {
+                    genre: 'Uniqe',
+                    name: 'Free Parking',
+                    img_src: 'https://miro.medium.com/max/713/1*ZOp-MtHxfrr_hONPaE8Hdw.jpeg',
+                },
+                {
+                    genre: 'Horror',
+                    name: 'Resident Evil 7',
+                    cost: 90,
+                    img_src: 'https://s3.gaming-cdn.com/images/products/1568/271x377/resident-evil-7-cover.jpg',
+                    color: 'LightSlateGray'
+                },
+                {
+                    genre: 'Horror',
+                    name: 'Dead by Daylight',
+                    cost: 50,
+                    img_src: 'https://justplayit.store/eefteems/2020/07/dead-by-daylight-steam-600x849.png',
+                    color: 'LightSlateGray'
+                },
+                //---------5(14)
+                {
+                    genre: 'Building',
+                    name: 'Sims 4',
+                    cost: 120,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1222670/header.jpg?t=1608238350',
+                    color:'Khaki'
+                },
+                {
+                    genre: 'Building',
+                    name: 'Cities: Skylines',
+                    cost: 20,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/255710/header.jpg?t=1606821413',
+                    color: 'Khaki'
+                },
+                {
+                    genre: 'Building',
+                    name: 'Planet Zoo',
+                    cost: 100,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/703080/header.jpg?t=1607429152',
+                    color: 'Khaki'
+                },
+                //-------6(17)
+                {
+                    genre: 'Uniqe',
+                    name: 'Go to Jail',
+                    img_src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP5ZqDRlHTQF5p54h38K0fN6qH57CZdH1wVg&usqp=CAU',
+                },
+                {
+                    genre: 'Strategy',
+                    name: 'Mount & blade 2',
+                    cost: 140,
+                    img_src: 'https://steamcdn-a.akamaihd.net/steam/apps/261550/header.jpg?t=1599656843',
+                    color: 'Tomato'
+                },
+                {
+                    genre: 'Strategy',
+                    name: 'Portal 2',
+                    cost: 50,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/620/header.jpg?t=1587582232',
+                    color: 'Tomato'
+                },
+                //--------7(20)
+                {
+                    genre: 'RPG',
+                    name: 'GTA V',
+                    cost: 100,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/271590/capsule_616x353.jpg?t=1592866696',
+                    color: 'CornflowerBlue'
+                },
+                {
+                    genre: 'RPG',
+                    name: 'DRAGON BALL Z',
+                    cost: 300,
+                    img_src: 'https://cdn.cloudflare.steamstatic.com/steam/apps/851850/header.jpg?t=1607545613',
+                    color: 'CornflowerBlue'
+                }, 
+                {    genre: 'RPG',
+                    name: 'NARUTO SHIPPUDEN',
+                    cost: 250,
+                    img_src: 'https://steamcdn-a.akamaihd.net/steam/apps/349040/capsule_616x353_thai.jpg?t=1598973980',
+                    color: 'CornflowerBlue'
+                },
+                //------8(23)
+                
+            ]
+            , 'logo_src': 'https://cdn.cloudflare.steamstatic.com/store/home/store_home_share.jpg'
+        },
+        {
+            'name': "mcDonald's",
+            'cards': [
+                {
+                    genre: 'Unique',
+                    name: 'Starting point',
+                    img_src: 'images/Go.jpg'
+                },
+                //--------go
+                {
+                    genre: 'Burgers',
+                    name: 'Big Mac Royal',
+                    cost: 300,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/img_1560257761387-0.png',
+                    color: 'IndianRed'
+                },
+                {
+                    genre: 'Unique',
+                    name: 'Tax',
+                    img_src: 'images/tax-card.jpg'
+                },
+                {
+                    genre: 'Burgers',
+                    name: 'Mac Royal',
+                    cost: 200,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/img_1468248208631-0.png',
+                    color: 'IndianRed'
+                },
+                //----1(3)
+                {
+                    genre: 'Sandwiches',
+                    name: 'Chicken Steak',
+                    cost: 200,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1468400271241-0.png',
+                    color: 'GreenYellow'
+                },
+                {
+                    genre: 'Sandwiches',
+                    name: 'Double fish Mac',
+                    cost: 230,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1583322178488-0.png',
+                    color: 'GreenYellow'
+                },
+                {
+                    genre: 'Uniqe',
+                    name: 'Jail',
+                    img_src: 'https://www.ultraboardgames.com/monopoly/gfx/ms11.jpg',
+                    color: 'MediumPurple'
+                },
+                //-----2(6)
+                {
+                    genre: 'Kids Meals',
+                    name: 'Mini Royal',
+                    cost: 100,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1473752414642-0.png',
+                    color: 'MediumPurple'
+                },
+                {
+                    genre: 'Kids Meals',
+                    name: 'Mini Crispy Chicken',
+                    cost: 90,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1473752414642-0.png',
+                    color: 'MediumPurple'
+                },
+                {
+                    genre: 'Kids Meals',
+                    name: 'Mini Fish Sandwiche',
+                    cost: 70,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1473752414642-0.png',
+                    color: 'MediumPurple'
+                },
+                //----------3(9)
+                {
+                    genre: 'Ice Cream',
+                    name: 'Vanilla Cone',
+                    cost: 50,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1470834680295-0.png',
+                    color: 'Gold'
+                },
+                {
+                    genre: 'Ice Cream',
+                    name: 'Sunday Carmel',
+                    cost: 100,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1470216169935-0.png',
+                    color: 'Gold'
+                },
+                {
+                    genre: 'Uniqe',
+                    name: 'Free Parking',
+                    img_src: 'https://miro.medium.com/max/713/1*ZOp-MtHxfrr_hONPaE8Hdw.jpeg',
+                },
+                //-------4(12)
+                {
+                    genre: 'Salads',
+                    name: 'Salads Siam With Crunchies',
+                    cost: 150   ,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1597157552529-0.png',
+                    color: 'LightSlateGray'
+                },
+                {
+                    genre: 'Salads',
+                    name: 'Green Salads With Nugets',
+                    cost: 170,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1532511437876-0.png',
+                    color: 'LightSlateGray'
+                },
+                {
+                    genre: 'Salads',
+                    name: 'Green Salads With Finger Fish',
+                    cost: 200,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1599053495641-0.png',
+                    color: 'LightSlateGray'
+                },
+                //-------5(15)
+                {
+                    genre: 'Shakes',
+                    name: 'Coconut Shake',
+                    cost: 120,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1595832113987-0.png',
+                    color: 'Khaki'
+                },
+                {
+                    genre: 'Shakes',
+                    name: 'Oreo Shake',
+                    cost: 120,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1576499683416-0.png',
+                    color: 'Khaki'
+                },
+                //------6(17)
+                {
+                    genre: 'Uniqe',
+                    name: 'Go to Jail',
+                    img_src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP5ZqDRlHTQF5p54h38K0fN6qH57CZdH1wVg&usqp=CAU',
+                },
+                {
+                    genre: 'Snacks',
+                    name: 'Finger Corn',
+                    cost: 170,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1473749751284-0.png',
+                    color: 'Tomato'
+                },
+                {
+                    genre: 'Snacks',
+                    name: 'Chicken Select',
+                    cost: 170,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1468325638046-0.png',
+                    color: 'Tomato'
+                },
+                //------7(20)
+                {
+                    genre: 'French Fries',
+                    name: 'French Fries',
+                    cost: 150,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1468421847053-0.png',
+                    color: 'CornflowerBlue'
+                },
+                {
+                    genre: 'French Fries',
+                    name: 'Sweet Potato French Fries',
+                    cost: 200,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1563775286863-0.png',
+                    color: 'CornflowerBlue'
+                }, 
+                {
+                    genre: 'French Fries',
+                    name: 'Rustic French Fries',
+                    cost: 150,
+                    img_src: 'https://www.mcdonalds.co.il/filestock/img/thumb_1474289484314-0.png',
+                    color: 'CornflowerBlue'
+                }, 
+                //------8(23)
+
+            ]
+            , 'logo_src': 'images/mcdonalds/logo_mc.png',
+        }
+    ]
+    ,"players": [
+        {
+            'name': 'player1',
+            'userName':"player1",
+            'money': 5000,
+            'location': 0,
+            'purchases': [],
+            'sum_of_purchases':0,
+            'img_src': "images/player1.png",
+            'color': 'red',
+            'worth': 5000,
+            'timeJail': 0,
+            'freeParking': false
+        },
+        {
+            'name': 'player2',
+            'userName': "player2",
+            'money': 5000,
+            'location': 0,
+            'purchases': [],
+            'sum_of_purchases': 0,
+            'img_src': "images/player2.png",
+            'color': 'blue',
+            'worth': 5000,
+            'timeJail': 0,
+            'freeParking': false
+
+        }
+    ]
+    , "GameRules": "<p>Players need to gets the heighest worth to win the game!</p><p>Player 1 alwayes start the game. In each turn the player thorw the dices if he thorw a double he get another turns!</p>" +
+        "<p>player can buy a card only if he at the location of the card and have enough money and the card havent been bought, after the purchase, a house will appear next to the price with the color of the player </p>" +
+        "<p><strong>Uniqe Cards</strong></p><p>Go to jail card: if player land on Go To Jail card he sent to jail at the left corner of the board for 3 turns. the player can continue the game if he thorw a double or tried 3 time, only on the next throw he we go out</p>" +
+        "<p>string point each time player went through the starting point he will get 200$ from the bank</p>" +
+        "<p>Free Parking: if player land on Free Parking card this player will lose a turn and the second player will play 2 turns in a row</p>" +
+        "<p>Pay Tax : if player land on Pay Tax card he will have to pay to the bank 10% of his money</p>" 
+}
